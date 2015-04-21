a449e2febaa84508597e0f7dd19325794c3ec04c
Jonathan Andrade <ndrd@ciencias.unam.mx>
2015-04-20 23:38:17 -0500

    first commit

---
+++ CMakeFiles/rainbown-sprawl.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/rainbown-sprawl.dir/rainbown-sprawl.cpp.o"
+  "rainbown-sprawl.pdb"
+  "rainbown-sprawl"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/rainbown-sprawl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
